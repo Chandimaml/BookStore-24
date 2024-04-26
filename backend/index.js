07175a12f06bf885ea2b3564b7e4daa5c00846ba
Chandimaml <chandimam98i@gmail.com>
2024-04-26 13:02:51 +0530

    update index.js file for retrive data function

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import { PORT , mongoDBURL} from './config.js';
 import mongoose from 'mongoose';
+import { Book } from './models/bookmodel.js'; 
 const app = express();
+
+//middleware for parsing request body
+app.use(express.json())
 
 app.get('/',(request,response)=>{
     console.log(request)
@@ -9,7 +13,48 @@ app.get('/',(request,response)=>{
 
 });
 
+//router for save new book
+app.post('/books', async(request,response)=>{
+  try{
+    if(
+      !request.body.title||
+      !request.body.author||
+      !request.body.publishYear
 
+    ){
+      return response.status(400).send({
+        message: 'send all required faileds:title,author,published year',
+      });
+    }
+    const newBook ={
+      title:request.body.title,
+      author:request.body.author,
+      publishYear:request.body.publishYear,
+    };
+    const book = await Book.create(newBook)
+    return response.status(201).send(book);
+
+  }catch(error){
+    console.log(error.message);
+    response.status(500).send({message:error.message});
+  }
+});
+
+//Route for get all book from database
+app.get('/books',async(requ,response)=>{
+  try{
+    const books =await Book.find({});
+
+    return response.status(200).json({
+      count : books.length,
+      data : books
+    });
+
+  }catch(error){
+    console.log(error.message);
+    response.status(500).send({message:error.message})
+  }
+})
 
 mongoose
   .connect(mongoDBURL)

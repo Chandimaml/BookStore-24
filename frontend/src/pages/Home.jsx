7054aac7eb64fe7b0ac49b9ca893da7d04a51e8f
Chandimaml <chandimam98i@gmail.com>
2024-04-27 09:21:37 +0530

    create Backbutton and modify createbook,Home and Show book pages.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -52,13 +52,13 @@ const Home = () =>{
                                 <td className='border border-slate-700 rounded-md max-md:hidden'>{book.publishYear}</td>
                                 <td className='border border-slate-700 rounded-md text-center'>
                                     <div className='flex justify-center grap-x-4'>
-                                        <Link to={`/books/detail/${book.id}`}>
+                                        <Link to={`/books/detail/${book._id}`}>
                                             <BsInfoCircle className='text-2xl text-green-800'/>
                                         </Link>
-                                        <Link to={`/books/edit/${book.id}`}>
+                                        <Link to={`/books/edit/${book._id}`}>
                                             <AiOutlineEdit className='text-2xl text-yellow-600'/>
                                         </Link>
-                                        <Link to={`/books/delete/${book.id}`}>
+                                        <Link to={`/books/delete/${book._id}`}>
                                             <MdOutlineDelete className='text-2xl text-red-600'/>
                                         </Link>
                                     </div>

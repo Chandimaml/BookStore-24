f86bdbe77a028a9033289013108549e64a8a8040
Chandimaml <chandimam98i@gmail.com>
2024-04-27 07:59:22 +0530

    create home reatbooks,edit ,delete ,list and modyfy index.js file

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,2 @@
 export const PORT = 5555;
-export const mongoDBURL = 'mongodb+srv://it20104962:it20104962Chandi@bookstore.bceh5i7.mongodb.net/?retryWrites=true&w=majority&appName=BookStore'
+export const mongoDBURL = 'mongodb+srv://it20104962:it20104962Chandi@bookstore.bceh5i7.mongodb.net/books-collection?retryWrites=true&w=majority&appName=BookStore'
---
+++ frontend/src/pages/DeleteBooks.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+const DeleteBooks = () =>{
+    return(
+        <div>DeleteBooks</div>
+    )
+}
+
+export default DeleteBooks;
